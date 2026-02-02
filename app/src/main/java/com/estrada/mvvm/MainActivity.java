72518124b44de5295018176ca23b5d31ced22374
reezicyb <jones.estrada3@example.com>
2026-02-02 11:52:38 +0800

    change UI

--- app/src/main/java/com/estrada/mvvm/MainActivity.java
+++ app/src/main/java/com/estrada/mvvm/MainActivity.java
@@ -20,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         items.add(new UserItem(R.drawable.user1, "Raiko Rainer Jones Estrada", "Last seen: 2 mins ago"));
         items.add(new UserItem(R.drawable.user2, "Mar Neil Jardenico", "Last seen: 1 hour ago"));
         items.add(new UserItem(R.drawable.user3, "Jay Mary Ompad", "Last seen: Dec 30"));
+        items.add(new UserItem(R.drawable.user4, "Quiela Marie Tacugue", "Last seen: Jan 15"));
+        items.add(new UserItem(R.drawable.user5, "Delmar Sosmena", "Last seen: 12 hours ago"));
+        items.add(new UserItem(R.drawable.user6, "Karlo Andrei Saren", "Active now"));
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new UserAdapter(this, items));
